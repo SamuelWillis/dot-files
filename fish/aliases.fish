5c78bbd7f46485f52f8aa171956c9edf97e7bc2e
SamuelWillis <samuel@echosec.net>
2016-08-04 13:07:18 -0700

    added in stash commands

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,7 +1,14 @@
 alias gd "git diff"
 alias ga "git add"
+alias gb "git branch"
+alias gp "git push"
+
 alias gaa "git add -A"
-alias gcs "git commit -a -m"
+alias gca "git commit -a -m"
 alias gcm "git commit -m"
 alias gst "git status"
-alias gp "git push"
+alias gco "git checkout"
+
+alias gss "git stash"
+alias gsp "git stash pop"
+alias gsl "git stash list"
